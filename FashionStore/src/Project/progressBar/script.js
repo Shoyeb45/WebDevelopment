ea158fa01be05612a1b7d3a77299996c0f212139
Shoyeb45 <shoyebff45@gmail.com>
2024-09-03 18:16:27 +0530

    rating and meter

---
+++ FashionStore/src/Project/progressBar/script.js
@@ -0,0 +1,22 @@
+arr = document.querySelectorAll("button");
+meteres = document.querySelectorAll("meter")
+let ratings = [0, 0, 0, 0 , 0];
+
+function doOnClick(x, arr, ratings) {
+    arr[x].onclick = () => {
+        ratings[x]++;
+        let sum = 0;
+    for(let i = 0; i < ratings.length; i++) {
+        sum += ratings[i];
+    }
+    for(let i = 0; i < ratings.length; i++) {
+        meteres[i].setAttribute("value", (ratings[i] * sum) / 100)
+    }
+    };
+}
+
+for(let i = 0; i < 5; i++) {
+    doOnClick(i, arr, ratings, meteres);
+}
+
+
