2241c43f87a6261813f962513ec7604154b7bfbb
Shoyeb45 <shoyebff45@gmail.com>
2024-12-22 01:01:27 +0530

    little update

--- LearningBackendNode/example-app-1/index.js
+++ LearningBackendNode/example-app-1/index.js
@@ -1,36 +1,27 @@
-export default function handler(req, res) {
-    res.status(200).json({ message: "Hello from Vercel!" });
-}
-
-  
-require("dotenv").config();
 const express = require("express");
+const { createServer } = require("http");
 
 const app = express();
-const port = 3000;
 
-// / is used for route or main, can be accessed as : https://localhost:portNumber
+// Define your routes
 app.get('/', (req, res) => {
     res.send("I am Shoyeb Ansari!");
 });
 
-// /twitter means in https://localhost:portNumber/twitter this path
 app.get('/twitter', (req, res) => {
     res.send("Shoyeb45");
 });
 
-// /login means in https://localhost:portNumber/login this path
 app.get("/login", (req, res) => {
-    res.send("<h1>Please login</h1>")
+    res.send("<h1>Please login</h1>");
 });
 
-// /youtube means in https://localhost:portNumber/youtube this path
 app.get("/youtube", (req, res) => {
-    res.send("<h2>Chai aur me</h2>")
+    res.send("<h2>Chai aur me</h2>");
 });
 
-
-app.listen(process.env.PORT, () => {
-    console.log("Example app listening on port", process.env.PORT);
-    
-})
+// Export the app as a Vercel serverless function
+module.exports = (req, res) => {
+    const server = createServer(app);
+    server.emit('request', req, res);
+};
