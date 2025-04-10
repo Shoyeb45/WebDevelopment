6f2d3a607072fd4fc1261bde969f868bd4901e9d
Shoyeb45 <shoyebff45@gmail.com>
2025-04-10 19:12:15 +0530

    100xdev: Middleware assignmet

---
+++ LearningBackendNode/middleware-assignments/03-errorCount.js
@@ -0,0 +1,36 @@
+import express from 'express';
+const app = express();
+let errorCount = 0;
+
+
+// You have been given an express server which has a few endpoints.
+// Your task is to
+// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
+// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
+
+
+app.get('/user', function (req, res) {
+    try {
+        throw new Error("User not found");
+        res.status(200).json({ name: 'john' });
+    } catch (error) {
+        console.error(error);
+        errorCount++;
+        res.status(404).json({ error });
+    }
+});
+
+app.post('/user', function (req, res) {
+    try {
+        res.status(200).json({ msg: 'created dummy user' });
+    } catch (error) {
+        console.error(error);
+        errorCount++;
+        res.status(404).json({ error });
+    }
+});
+
+app.get('/errorCount', function (req, res) {
+    res.status(200).json({ errorCount });
+});
+app.listen(3000);
