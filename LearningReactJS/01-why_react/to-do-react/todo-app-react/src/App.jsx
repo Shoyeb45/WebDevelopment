a219c17e45339d4abfeae64299e704e34fce3dee
Shoyeb45 <shoyebff45@gmail.com>
2025-04-15 07:58:59 +0530

    Refactor todo app: Implement functional components and state management with hooks; remove legacy HTML and JavaScript code.

--- LearningReactJS/01-why_react/to-do-react/todo-app-react/src/App.jsx
+++ LearningReactJS/01-why_react/to-do-react/todo-app-react/src/App.jsx
@@ -1,14 +1,73 @@
 import { useState } from "react";
-import "./App.css";
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodo] = useState([]);
 
   return (
     <div>
-      Hii Hammad
+      <InputCompo todos={todos} setTodo={setTodo} />
+      {todos.map((todo) => {
+        return <Todo title={todo.title} description={todo.description} />;
+      })}
     </div>
   );
 }
 
+function InputCompo(props) {
+  // State variable of title and description
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  function addTodo(todo) {
+    props.setTodo([
+      ...props.todos,
+      {
+        title: title,
+        description: description,
+      },
+    ]);
+    setTitle("");
+    setDescription("");
+  }
+
+  return (
+    <div>
+      <dt>Todo Title:</dt>
+      <dd>
+        <input 
+          type="text" 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)} 
+        />
+      </dd>
+
+      <dt>Todo Description:</dt>
+      <dd>
+        <input 
+          type="text" 
+          value={description}
+          onChange={(e) => setDescription(e.target.value)} 
+        />
+      </dd>
+      <dd>
+        <button onClick={addTodo}>Add Todo</button>
+      </dd>
+    </div>
+  );
+}
+
+/**
+ *
+ * @param {*} props : {title, description}
+ */
+function Todo(props) {
+  return (
+    <>
+      <h1>{props.title}</h1>
+      <h2>{props.description}</h2>
+    </>
+  );
+}
 export default App;
