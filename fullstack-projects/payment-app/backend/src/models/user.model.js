6f35ca867b164ab3eca8e2716830098989581884
Shoyeb45 <shoyebff45@gmail.com>
2025-04-20 09:24:34 +0530

    user schema

---
+++ fullstack-projects/payment-app/backend/src/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        index: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    }, 
+    lastName: {
+        type: String,
+        required: true,
+    }, 
+    email: {
+        type: String,
+        required: true,
+    },
+    refreshToken: {
+        type: String,
+    }
+}, { timestamps : true});
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign({
+        _id: this._id, // id from database  
+        email: this.email,
+        username: this.username,
+    }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "7d" });
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign({
+        _id: this._id, // id from database  
+    }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "30d" });
+}
+
+export const User = mongoose.model("User", userSchema);
+
